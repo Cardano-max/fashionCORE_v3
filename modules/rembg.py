4d18ca491a653e2c973b6d54ecef647ed78c067d
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2024-07-02 02:00:58 +0500

    Productionize a new virtual try-on func Test44

--- modules/rembg.py
+++ modules/rembg.py
@@ -1,15 +1,3 @@
-from rembg import remove
-import gradio as gr
-from PIL import Image
-import numpy as np
-
-def rembg_run(input_image, progress=gr.Progress(track_tqdm=True)):
-    if isinstance(input_image, np.ndarray):
-        input_image = Image.fromarray(input_image.astype('uint8'), 'RGB')
-    elif isinstance(input_image, str):
-        input_image = Image.open(input_image)
-    else:
-        raise ValueError("Input must be either a numpy array or a file path")
-    
-    output = remove(input_image)
-    return np.array(output)
+def rembg_run(input_image):
+    from rembg import remove
+    return remove(input_image)
