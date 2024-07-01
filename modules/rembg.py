85f9ac16c39d3e32221dd9a1c400994cca430e01
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2024-07-01 15:54:59 +0500

    Productionize a new virtual try-on func Test6

--- modules/rembg.py
+++ modules/rembg.py
@@ -1,8 +1,10 @@
 from rembg import remove
 import gradio as gr
 from PIL import Image
+import numpy as np
 
-def rembg_run(path, progress=gr.Progress(track_tqdm=True)):
-    input = Image.open(path)
-    output = remove(input)
-    return output
+def rembg_run(input_image, progress=gr.Progress(track_tqdm=True)):
+    if isinstance(input_image, np.ndarray):
+        input_image = Image.fromarray(input_image)
+    output = remove(input_image)
+    return np.array(output)
