06dd9b0840cd945d061451926281974566fb74c5
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2024-07-04 15:10:34 +0500

    Testing Pahse 54

--- webui2.py
+++ webui2.py
@@ -120,6 +120,7 @@ def virtual_try_on(clothes_image, person_image, inpaint_mask):
 # Example garment images (replace with actual image paths)
 example_garments = [
     "/Users/ateeb.taseer/fashionCORE_v3/fashionCORE_v3/images/first.png",
+    "/Users/ateeb.taseer/fashionCORE_v3/fashionCORE_v3/images/first.png",
     "/Users/ateeb.taseer/fashionCORE_v3/fashionCORE_v3/images/second.png",
     "/Users/ateeb.taseer/fashionCORE_v3/fashionCORE_v3/images/third.png",
 ]
