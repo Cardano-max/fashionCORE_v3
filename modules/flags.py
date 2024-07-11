38e6366010ff835952e9cd00b5cca6d274d0ad5f
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2024-07-11 15:32:10 +0500

    Dev-46

--- modules/flags.py
+++ modules/flags.py
@@ -7,6 +7,7 @@ strong_variation = 'Vary (Strong)'
 upscale_15 = 'Upscale (1.5x)'
 upscale_2 = 'Upscale (2x)'
 upscale_fast = 'Upscale (Fast 2x)'
+from typing import Union
 
 uov_list = [
     disabled, subtle_variation, strong_variation, upscale_15, upscale_2, upscale_fast
